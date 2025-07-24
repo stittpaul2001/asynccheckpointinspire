3e27ae06127580337ab32659242498c7f27be9bc
stittpaul2001 <stittpaul2001@gmail.com>
2025-07-24 12:49:03 -0600

    Got my images loading in my console, and pulling to my screen. As well as adding in a little bit of styling

--- app/controllers/ImagesController.js
+++ app/controllers/ImagesController.js
@@ -1,7 +1,33 @@
+import { AppState } from "../AppState.js"
+import { imagesService } from "../services/ImagesService.js"
 
 export class ImagesController {
 
   constructor() {
+    AppState.on('images', this.drawImages)
+    this.getImages()
+  }
+
+  async getImages() {
+    try {
+      await imagesService.getImages()
+    } catch (error) {
+      console.error(error)
+
+      // @ts-ignore
+      Pop.error(error, 'Could not get images!!!')
+
+    }
+  }
+
+  drawImages() {
+    const images = AppState.images
+    let imagesContent = images.attribution
+    const imagesElem = document.getElementById('images')
+    // @ts-ignore
+    imagesElem.innerHTML = imagesContent
+    document.body.style.backgroundImage = `url(${images.imgUrls.regular})`
+
   }
 
 
--- app/services/ImagesService.js
+++ app/services/ImagesService.js
@@ -1,6 +1,15 @@
+import { AppState } from "../AppState.js"
+import { Images } from "../models/Images.js"
+import { api } from "./AxiosService.js"
 
 class ImagesService {
 
+  async getImages() {
+    const response = await api.get('api/images')
+    console.log('images', response.data)
+    const images = new Images(response.data)
+    AppState.images = images
+  }
 
 
 }
